2034dabd78f19e21f832f7ef1e98f989d0574a37
Victoria Sydow <vs222vs@student.lnu.se>
2026-02-02 23:44:04 +0100

    Börjat på menyn. Snart klar

--- ConsoleApplication1/main.c
+++ ConsoleApplication1/main.c
@@ -31,7 +31,72 @@ int main() {
     EventQueue* queue = queue_create(10);
     EventLog* log = log_create(10);
 
+        int choice;
+        int run = 1;
+
+        while (run) {
+            printf("\n1) Tick\n");
+            printf("2) Print\n");
+            printf("3) Sort\n");
+            printf("4) Find\n");
+            printf("5) Help\n");
+            printf("6) Exit\n");
+            printf("Val: ");
+            scanf("%d", &choice);
+
+            switch (choice) {
+            case 1: {
+                int n;
+                printf("Antal tick: ");
+                scanf("%d", &n);
+                tick(n, queue, log);
+                break;
+            }
+            case 2: {
+                for (int i = 0; i < log_size(log); i++) {
+                    Event e = log_get(log, i);
+                    printf("Timestamp: %d, Sensor: %d, Type: %d, Value: %d\n",
+                        e.timestamp, e.sensorId, e.type, e.value);
+                break;
+            }
+            case 3: {
+                printf("1) Insertion sort\n");
+                int sortVal;
+                scanf("%d", &sortVal);
+                if (sortVal == 1) {
+                    sort_insertion(log);
+                }
+                break;
+            }
+            case 4: {
+                int sensorId;
+                printf("SensorId: ");
+                scanf("%d", &sensorId);
+                // gör find här sedan
+                break;
+            }
+            case 5: {
+                  printf("1) Tick - kör n iterationer\n");
+                  printf("2) Print - skriv ut loggen\n");
+                  printf("3) Sort - sortera loggen\n");
+                  printf("4) Find - sök efter sensorId\n");
+                  printf("5) Help - visa hjälp\n");
+                  printf("6) Exit - avsluta\n");
+                  break;
+            }
+            case 6: {
+                run = 0;
+                break;
+            }
+        }
+
+        queue_destroy(queue);
+        log_destroy(log);
+        return 0;
+    }
+
     queue_destroy(queue);
     log_destroy(log);
     return 0;
+        
 }
